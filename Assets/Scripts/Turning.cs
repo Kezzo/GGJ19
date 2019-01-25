1f865b36d5da233d69cba3c7a4adb5269efba7fb
Erkki Koskenkorva <erkki@futureplay.fi>
2019-01-25 19:26:04 +0700

    Turning script for swapping the sprite direction

---
+++ Assets/Scripts/Turning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turning : MonoBehaviour 
+{
+	[SerializeField] private float HitboxHeight = 30f;
+	[SerializeField] private float HitboxWidth = 0.8f;
+
+	private Vector3 parentScale;
+
+    void Awake()
+    {
+        BoxCollider2D bc;
+        bc = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+        bc.size = new Vector2( HitboxWidth, HitboxHeight );
+        bc.isTrigger = true;
+		parentScale = transform.parent.localScale;
+    }
+	
+	void OnTriggerExit2D( Collider2D col )
+	{
+		if( col.gameObject.name == "player1" || col.gameObject.name == "player2" )
+		{
+			if( col.transform.position.x < transform.position.x )
+				transform.parent.localScale = new Vector3( -parentScale.x, parentScale.y, parentScale.z );
+			else
+				transform.parent.localScale = parentScale;
+		}
+	}
+}
